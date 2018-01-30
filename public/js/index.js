db3ee861a121714a50d3aa505155ed7ab0b57735
René Serulle <kamil@Renes-MacBook-Pro.local>
2018-01-29 21:11:36 -0500

    Add css for chat page

--- public/js/index.js
+++ public/js/index.js
@@ -35,11 +35,13 @@ socket.on('newLocationMessage', function (message) {
 jQuery('#message-form').on('submit', function (e) {
     e.preventDefault();
 
+    var messageTextbox = $('[name=message]');
+
     socket.emit('createMessage', {
         from: 'User',
-        text: jQuery('[name=message]').val()
+        text: messageTextbox.val()
     }, function () {
-
+        messageTextbox.val('');
     });
 });
 
@@ -47,12 +49,16 @@ var locationButton = $('#send-location');
 locationButton.on('click', function () {
     if (!navigator.geolocation) alert('Geolocation not support by this browser');
 
+    locationButton.attr('disabled', 'disabled').text('sending location...');
+
     navigator.geolocation.getCurrentPosition(function (position) {
+        locationButton.removeAttr('disabled').text('send location');
         socket.emit('createLocationMessage', {
             latitude: position.coords.latitude,
             longitude: position.coords.longitude
         });
     }, function () {
+        locationButton.removeAttr('disabled').text('send location');
         alert('Unable to fetch location.');
     });
 });
