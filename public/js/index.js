24ecee6a6c0a8fc126600d95d328c0073891a916
René Serulle <kamil@Renes-MBP.nyc.rr.com>
2018-01-30 20:40:32 -0500

    Scroll to bottom if user is close to bottom

--- public/js/index.js
+++ public/js/index.js
@@ -1,5 +1,22 @@
 var socket = io();
 
+function scrollToBottom () {
+    // selectors
+    var messages = $('#messages');
+    var newMessage = messages.children('li:last-child');
+
+    // heights
+    var clientHeight = messages.prop('clientHeight');
+    var scrollTop = messages.prop('scrollTop');
+    var scrollHeight = messages.prop('scrollHeight');
+    var newMessageHeight = newMessage.innerHeight();
+    var lastMessageHeight = newMessage.prev().innerHeight();
+
+    if (clientHeight + scrollTop + newMessageHeight + lastMessageHeight >= scrollHeight) {
+        messages.scrollTop(scrollHeight);
+    }
+}
+
 socket.on('connect', function () {
     console.log('Connected to server');
     
@@ -24,6 +41,7 @@ socket.on('newMessage', function (message) {
     });
 
     $('#messages').append(html);
+    scrollToBottom();
 });
 
 socket.on('newLocationMessage', function (message) {
@@ -37,6 +55,7 @@ socket.on('newLocationMessage', function (message) {
     });
 
     $('#messages').append(html);
+    scrollToBottom();
 });
 
 jQuery('#message-form').on('submit', function (e) {
